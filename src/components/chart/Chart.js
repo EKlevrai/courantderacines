3efe2de48fb805e849968570f6b4a669199bd9f9
Olivier Ripart <olivier.ripart@live.fr>
2017-07-30 07:49:23 +0200

    [barchart] added tabs

--- src/components/chart/Chart.js
+++ src/components/chart/Chart.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Row, Col } from 'react-bootstrap';
+import { Row, Col, Tabs, Tab } from 'react-bootstrap';
 
 import Pie from './charts/Pie';
 
@@ -9,7 +9,16 @@ class Chart extends Component {
 			<Row>
 				<Col md={2}></Col>
 				<Col md={8}>
-					<Pie/>
+					<Tab.Container defaultActiveKey={'pie'} animation={false}  generateChildId={(eventKey, type) => `${type}-${eventKey}`}>
+						<Tabs id="noanim-tab-example">
+							<Tab eventKey={'pie'} title="Donut/Pie">
+								<Pie/>
+							</Tab>
+							<Tab eventKey={'bar'} title="Bars">
+
+							</Tab>
+						</Tabs>
+					</Tab.Container>
 				</Col>
 				<Col md={2}></Col>
 			</Row>
