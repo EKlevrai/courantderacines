105acd9c3cd9027150575746b1377f29127f6e47
Olivier Ripart <olivier.ripart@live.fr>
2017-07-30 07:13:36 +0200

    [chart] finalizing toggles

--- src/components/chart/charts/Pie.js
+++ src/components/chart/charts/Pie.js
@@ -22,6 +22,49 @@ let r = 100;
 let svg;
 
 class PieChart extends Component {
+
+	/**
+		* toggle type (donut to pie and vice-versa)
+		*	@param {integer} val : 1 for donut, other for pie
+		*/
+	 toggleType(val) {
+		let r2;
+		if(val === 1){
+			r2 = r/2;
+		} else {
+			r2 = 0;
+		}
+		svg.selectAll("path")
+		.transition()
+		.duration(500)
+		.attr("d",d3.svg.arc()
+			.innerRadius(r2)
+			.outerRadius(r));
+	}
+
+	/**
+		* toggle color
+		*	@param {integer} val : 1 for red, 2 for green, 3 for blue
+		*/
+	toggleColor(val) {
+		let color;
+		if(val === 1){
+			color = ['#7a1f1f', '#cceaae'];
+		}
+		if(val === 2){
+			color = ['#4d8217', '#cceaae'];
+		}
+		if(val === 3){
+			color = ['#1f5c7a', '#aed6ea'];
+		}
+		const colors = d3.scale.linear().domain([0,PieChartData.length]).range(color);
+		svg.selectAll("path")
+		.transition()
+		.duration(1000)
+		.style("fill", (d, i) => colors(i));
+	}
+
+
 	componentDidMount() {
 		this.setState({
 			index: -1
@@ -42,10 +85,10 @@ class PieChart extends Component {
 		svg.selectAll("path")
 		  .data(d3.layout.pie())
 		  .enter().append("path")
-		    .attr("d", d3.svg.arc()
-		      .innerRadius(r / 2)
-		      .outerRadius(r))
-					.style("fill", (d, i) => z(i))
+			.attr("d", d3.svg.arc()
+				.innerRadius(r / 2)
+				.outerRadius(r / 2))
+				.style("fill", (d, i) => z(i))
 				.on("mouseover", (d, i) => {
 					ToolTip.on(tooltip, data[i].label);
 				})
@@ -57,47 +100,8 @@ class PieChart extends Component {
 						index: i,
 					})
 				});
-	}
+				this.toggleType(1);
 
-	/**
-		* toggle type (donut to pie and vice-versa)
-		*	@param {integer} val : 1 for donut, other for pie
-		*/
-	toggleType = (val) => {
-		let r2;
-		if(val === 1){
-			r2 = r/2;
-		} else {
-			r2 = 0;
-		}
-		svg.selectAll("path")
-		.transition()
-		.duration(200)
-		.attr("d",d3.svg.arc()
-			.innerRadius(r2)
-			.outerRadius(r));
-	}
-
-	/**
-		* toggle color
-		*	@param {integer} val : 1 for red, 2 for green, 3 for blue
-		*/
-	toggleColor = (val) => {
-		let color;
-		if(val === 1){
-			color = ['#7a1f1f', '#cceaae'];
-		}
-		if(val === 2){
-			color = ['#4d8217', '#cceaae'];
-		}
-		if(val === 3){
-			color = ['#1f5c7a', '#aed6ea'];
-		}
-		const colors = d3.scale.linear().domain([0,PieChartData.length]).range(color);
-		svg.selectAll("path")
-		.transition()
-		.duration(1000)
-		.style("fill", (d, i) => colors(i));
 	}
 
 	render() {
@@ -107,8 +111,8 @@ class PieChart extends Component {
 				<Col md={3}>
 						<div id={'pie'}/>
 						<div className={'toggles'}>
+							<h3>Chart type</h3>
 							<p>
-								<h3>Chart type</h3>
 								<ToggleButtonGroup type={'radio'} name="donut/pie" defaultValue={1} onChange={this.toggleType}>
 		 							<ToggleButton value={1}>
 			 							Donut
@@ -118,8 +122,8 @@ class PieChart extends Component {
 									</ToggleButton>
 	 							</ToggleButtonGroup>
 							</p>
+							<h3>Chart color</h3>
 							<p>
-								<h3>Chart color</h3>
 								<ToggleButtonGroup type={'radio'} name="color" defaultValue={3} onChange={this.toggleColor}>
 		 							<ToggleButton value={1}>
 			 							Red
