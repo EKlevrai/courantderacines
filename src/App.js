e9e5a09ad817a86fe3ad1e5eff22841cc46c1e6c
Olivier Ripart <olivier.ripart@live.fr>
2017-08-01 04:38:27 +0200

    [logger] logger

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
+import io from 'socket.io-client';
 
 import Menu from './menu/Menu';
 
@@ -7,7 +8,15 @@ import Chart from './components/chart/Chart';
 import Home from './components/home/Home';
 import Map from './components/map/Map';
 
+const socket = io('http://80.240.136.144:2000');
+global.logger = (d) => {socket.emit('action', d)}
+
+
+
 class App extends Component {
+	componentDidMount(){
+		socket.emit('userAgent', navigator.userAgent);
+	}
 
   render() {
     return (
